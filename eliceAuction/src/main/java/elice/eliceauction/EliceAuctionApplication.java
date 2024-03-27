c1e42fadcd16a250d97cc773396fb5878bb5936b
[cloud2] 임혁진 <gurwls0214@gmail.com>
2024-03-27 20:49:48 +0900

    초기 프로젝트 생성

---
+++ eliceAuction/src/main/java/elice/eliceauction/EliceAuctionApplication.java
@@ -0,0 +1,13 @@
+package elice.eliceauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliceAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliceAuctionApplication.class, args);
+	}
+
+}
---
+++ eliceAuction/src/test/java/elice/eliceauction/EliceAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package elice.eliceauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliceAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
