8de16a86f6f2b400590950878604f4a60c7508f2
hojin0729 <0918syj@gmail.com>
2024-04-03 09:49:33 +0900

    [Feat] 주문 도메인 service, controller 생성

--- eliceAuction/src/main/java/elice/eliceauction/domain/auction/entity/UserAddress.java
+++ eliceAuction/src/main/java/elice/eliceauction/domain/auction/entity/UserAddress.java
@@ -1,9 +1,12 @@
 package elice.eliceauction.domain.auction.entity;
 
+import elice.eliceauction.domain.product.entity.Product;
+import elice.eliceauction.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
@@ -11,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserAddress {
@@ -21,17 +25,20 @@ public class UserAddress {
     @Column(name = "name")
     private String name;
 
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(name = "rn_address", nullable = false)
-    private String rn_address;
+    @Column(name = "address", nullable = false)
+    private String address;
 
-    public void UserAddress(String name, User user, String rn_address){
+    public void UserAddress(String name, User user, String address){
         this.name = name;
         this.user = user;
-        this.rn_address = rn_address;
+        this.address = address;
     }
     @OneToOne(mappedBy = "user_address_id")
     private Product product;
