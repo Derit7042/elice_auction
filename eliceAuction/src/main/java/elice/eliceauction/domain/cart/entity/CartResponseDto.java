d8871e30a4062e6cc77612c82ae66d30b1e9e801
[cloud2] 임혁진 <gurwls0214@gmail.com>
2024-04-11 20:54:54 +0900

    feat: [Cart] 장바구니 기초 페이지 구현 및 데이터 출력

--- eliceAuction/src/main/java/elice/eliceauction/domain/cart/entity/CartResponseDto.java
+++ eliceAuction/src/main/java/elice/eliceauction/domain/cart/entity/CartResponseDto.java
@@ -3,11 +3,17 @@ package elice.eliceauction.domain.cart.entity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Schema(description = "장바구니 응답 DTO")// 스웨거 어노테이션
 @Getter
+@Setter
 @Builder
 public class CartResponseDto {
+
+    @Schema(description = "상품 id")
+    Long productId;
+
     @Schema(description = "작품명")
     String title;
     @Schema(description = "작품 가격")
