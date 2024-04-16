d09df6e07d5de29e904b5827f20b9e86a191b4d3
YachaTree <dnftksghkfkd@icloud.com>
2024-04-16 18:03:12 +0900

    refactor : client token 로직 수정

--- eliceAuction/src/main/java/elice/eliceauction/domain/member/controller/MemberViewController.java
+++ eliceAuction/src/main/java/elice/eliceauction/domain/member/controller/MemberViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/members")
+@RequestMapping("/members")
 public class MemberViewController {
 
     @GetMapping("/register")
