380bd59893e263e052e6b677e887d7e52fbf4a76
YachaTree <dnftksghkfkd@icloud.com>
2024-04-17 17:09:03 +0900

    refactor : memberSignDto 수정

--- eliceAuction/src/main/java/elice/eliceauction/domain/member/dto/MemberSignUpDto.java
+++ eliceAuction/src/main/java/elice/eliceauction/domain/member/dto/MemberSignUpDto.java
@@ -1,8 +1,21 @@
 package elice.eliceauction.domain.member.dto;
 
 import elice.eliceauction.domain.member.entity.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-public record MemberSignUpDto(String username, String password, String name) {
+public record MemberSignUpDto(@NotBlank(message = "아이디를 입력해주세요") @Size(min = 2, max = 25, message = "아이디는 2~25자 내외로 입력해주세요")
+                              String username,
+
+                              @NotBlank(message = "비밀번호를 입력해주세요")
+                              @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{2,30}$",
+                                      message = "비밀번호는 2~30 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
+                              String password,
+
+                              @NotBlank(message = "이름을 입력해주세요") @Size(min=2, message = "사용자 이름이 너무 짧습니다.")
+                              @Pattern(regexp = "^[A-Za-z가-힣]+$", message = "사용자 이름은 한글 또는 알파벳만 입력해주세요.")
+                              String name) {
 
     public Member toEntity() {
         return Member.builder().username(username).password(password).name(name).build();
