875c4fdebe1c1a38c13ef402e793fdc32d3986df
[cloud2] 임혁진 <gurwls0214@gmail.com>
2024-04-17 11:12:56 +0900

    feat: [member] 공통 API post 요청시 jwt 반환받을 수 있는 옵션 추가

--- eliceAuction/src/main/resources/static/api.js
+++ eliceAuction/src/main/resources/static/api.js
@@ -4,7 +4,14 @@ const baseUrl = "http://localhost:8080/api";  // 테스트 모드
 // 헤더를 구성하는 함수
 function configureHeaders() {
     const token = sessionStorage.getItem("token");
-    return token && token.trim() !== "" ? { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" } : { "Content-Type": "application/json" };
+    let headers;
+
+    headers = { "Content-Type": "application/json" };
+
+    if(token && token.trim() !== "") {
+        headers["Authorization"]=`Bearer ${token}`;
+    }
+    return headers;
 }
 
 // GET 요청
@@ -26,7 +33,7 @@ async function get(endpoint, params = "") {
 }
 
 // POST 요청
-async function post(endpoint, data) {
+async function post(endpoint, data, returnJWT = false) {
     const apiUrl = `${baseUrl}${endpoint}`;
     console.log(`POST 요청: ${apiUrl}`);
 
@@ -40,12 +47,40 @@ async function post(endpoint, data) {
             const errorDetails = await response.json();
             throw new Error(`HTTP 상태 코드: ${response.status}, 오류 메시지: ${errorDetails.message}`);
         }
-        return response.headers.get("Content-Length") !== "0" ? await response.json() : { message: "성공적으로 처리되었습니다." };
+
+        // JWT 추출 및 반환
+        const jwt = response.headers.get("Authorization");
+        if (returnJWT && jwt) {
+            return jwt;
+        }
+
+        return await response.json();
     } catch (error) {
         console.error("POST 요청 실패:", error);
         throw error;
     }
 }
+// async function post(endpoint, data) {
+//     const apiUrl = `${baseUrl}${endpoint}`;
+//     console.log(`POST 요청: ${apiUrl}`);
+//
+//     try {
+//         const response = await fetch(apiUrl, {
+//             method: "POST",
+//             headers: configureHeaders(),
+//             body: JSON.stringify(data)
+//         });
+//         if (!response.ok) {
+//             const errorDetails = await response.json();
+//             throw new Error(`HTTP 상태 코드: ${response.status}, 오류 메시지: ${errorDetails.message}`);
+//         }
+//         return await response.json();
+//         // return response.headers.get("Content-Length") !== "0" ? await response.json() : { message: "성공적으로 처리되었습니다." };
+//     } catch (error) {
+//         console.error("POST 요청 실패:", error);
+//         throw error;
+//     }
+// }
 
 // PATCH 요청
 async function patch(endpoint, params = "", data) {
